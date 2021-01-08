ccc089e6f4cc4fcd166599831c5b0a696acfdad3
ztwu4 <ztwu4@iflytek.com>
2021-01-08 16:07:29 +0800

    优化

--- src/main/java/com/iflytek/edu/hbase/test/PartitionRowKey/PartitionRowKeyManager.java
+++ src/main/java/com/iflytek/edu/hbase/test/PartitionRowKey/PartitionRowKeyManager.java
@@ -27,11 +27,26 @@ public class PartitionRowKeyManager implements RowKeyGenerator,
         for(int i = 1; i < partition ; i ++) {
             splitKeys[i-1] = Bytes.toBytes((long)i);
         }
+
         /**
+         * rowkey=0002rer4343343422,则当前这条数据就会保存到0001|~0002|这个region里，
+         * 因为我的messageId都是字母+数字，“|”的ASCII值大于字母、数字。
          *
-         * [0,1)***
-         * [1,2)***
-         * [2,3)***
+         */
+        /**
+         * splitkeys = [1,2,3,4]
+                     * [, 1)*** 0xx, <1
+                     * [1,2)*** 1xx, <2
+                     * [2,3)*** 2xx, <3 <=== rowkey=0002rer4343343422
+                     * [3,4)*** 3xx, <4
+                     * [4, )*** 4xx
+         *
+         * splitkeys = [1|,2|,3|,4|]
+         *          * [,1|)***  0xx, <1|xx
+         *          * [1|,2|)*** >1|xx , <2|xx <=== rowkey=0002rer4343343422
+         *          * [2|,3|)***
+         *          * [3|,4|)***
+         *          * [4|,)***
          *
          */
         return splitKeys;
